ebf03983a1c429887083a0940244603801795e16
Your Name <18311017120@163.com>
2019-06-28 18:45:21 +0800

    店铺搜索不可以清楚

--- src/pages/order/index.vue
+++ src/pages/order/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="order-page">
     <el-form :inline="true" ref="searchForm" :model="searchData" class="header-search">
-      <el-form-item label="所属店铺：" prop="shopId">
+      <el-form-item label="所属店铺：">
         <el-select v-model="searchData.shopId" placeholder="请选择">
           <el-option v-for="(item) in shopList" :key="item.shopId" :label="item.shopName" :value="item.shopId"></el-option>
         </el-select>
@@ -21,7 +21,7 @@
           <el-option v-for="(name, id) in orderStatus" :key="id" :label="name" :value="id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="日期筛选：" prop="time">
+      <el-form-item label="日期筛选：">
         <el-date-picker size="small" v-model="searchData.time" type="daterange" align="right" unlink-panels range-separator="~" start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd" :default-time="['00:00:00', '23:59:59']">
         </el-date-picker>
       </el-form-item>
