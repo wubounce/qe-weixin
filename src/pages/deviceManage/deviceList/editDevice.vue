5fb0a5c10450e75386f005a0cdcbbba0a0d9ca16
Your Name <18311017120@163.com>
2019-08-29 14:51:38 +0800

    1

--- src/pages/deviceManage/deviceList/editDevice.vue
+++ src/pages/deviceManage/deviceList/editDevice.vue
@@ -309,13 +309,13 @@ export default {
       let reg2 = /^(([0-9]|[1-9][0-8])(\.\d{0,3})?|(([1-8][0-9])(\.\d{0,3})?)|0\.[0-9]{0,3}|99|99.0|99.00|99.000)$/;
       if ((this.deviceEditForm.support & 2) === 2) {
         if (!reg2.test(value)) {
-          return callback(new Error(`请输入0.001-99之间的数字,最多保留3位小数`));
+          return callback(new Error(`请输入0-99之间的数字,最多保留3位小数`));
         } else {
           callback();
         }
       } else {
         if (!reg1.test(value)) {
-          return callback(new Error(`请输入0.01-99之间的数字,最多保留2位小数`));
+          return callback(new Error(`请输入0-99之间的数字,最多保留2位小数`));
         } else {
           callback();
         }
@@ -328,9 +328,9 @@ export default {
       deviceEditFormRules: {
         machineName: [{ required: true, message: '请输入设备名称', trigger: 'blur' }],
         needMinutes: [{ required: true, message: '请输入耗时', trigger: 'blur' }, { pattern: /^([1-9]\d{0,4})$/, message: '请输入1-9999之间的数字', trigger: 'blur' }],
-        waterMachineNeedMinutes: [{ required: true, message: '请输入单脉冲流量', trigger: 'blur' }, { pattern: /^(([0-9]|([1-9][0-9]{1,2}([0-8])?)|([1-9][0-8]{1,2}([0-9])?))(\.\d{0,3})?|0\.\d{0,3}|9999|9999.0|9999.00|9999.000)$/, message: '请输入0.001-9999之间数字，最多保留3位小数', trigger: 'blur' }],
-        functionPrice: [{ required: true, message: '请输入原价', trigger: 'blur' }, { pattern: /^(([0-9]|[1-9][0-8])(\.\d{0,2})?|(([1-8][0-9])(\.\d{0,2})?)|0\.[1-9]{0,2}|99|99.0|99.00)$/, message: '请输入0.01-99之间的数字,最多保留2位小数', trigger: 'blur' }],
+        functionPrice: [{ required: true, message: '请输入原价', trigger: 'blur' }, { pattern: /^(([0-9]|[1-9][0-8])(\.\d{0,2})?|(([1-8][0-9])(\.\d{0,2})?)|0\.[1-9]{0,2}|99|99.0|99.00)$/, message: '请输入0-99之间的数字,最多保留2位小数', trigger: 'blur' }],
         waterMachinePirce: [{ required: true, message: '请输入价格', trigger: 'blur' }, { validator: validatorWterMachinePirce, trigger: 'blur' }],
+        waterMachineNeedMinutes: [{ required: true, message: '请输入单脉冲流量', trigger: 'blur' }, { pattern: /^(([1-9]|([1-9][0-9]{1,2}([0-8])?)|([1-9][0-8]{1,2}([0-9])?))(\.\d{0,3})?|0\.\d{0,3}|9999|9999.0|9999.00|9999.000)$/, message: '请输入0.001-9999之间数字，最多保留3位小数', trigger: 'blur' }],
         chargeMachinePirce: [{ required: true, message: '请输入价格', trigger: 'blur' }, { pattern: /^([0-4]{1}([.]{1}[0-9]{1,2})?|5|5.0|5.00)$/, message: '请输入0.01-5之间数字，最多保留2位小数', trigger: 'blur' }],
         functionCode: [{ required: true, message: '请输入脉冲', trigger: 'blur' }, { pattern: /^([1-9]\d{0,1})$/, message: '请输入1-99之间的数字', trigger: 'blur' }],
         detergentLiquid: [{ required: true, message: '请输入用量', trigger: 'blur' }, { pattern: /^([1-9]\d{0,1})$/, message: '请输入1-99之间的数字', trigger: 'blur' }],
@@ -338,8 +338,8 @@ export default {
         'extraAttr.power1': [{ required: true, message: '请输入功率', trigger: 'blur' }, { validator: validatorPower1, trigger: 'blur' }],
         'extraAttr.power2': [{ required: true, message: '请输入功率', trigger: 'blur' }, { validator: validatorPower2, trigger: 'blur' }],
         'extraAttr.power3': [{ required: true, message: '请输入功率', trigger: 'blur' }, { validator: validatorPower3, trigger: 'blur' }],
-        'extraAttr.ratio2': [{ required: true, message: '请输入系数', trigger: 'blur' }, { pattern: /^((0\.[1-9]{0,1}?)|1|1.0|0)$/, message: '请输入0.1-1之间的数字，最多保留1位小数', trigger: 'blur' }],
-        'extraAttr.ratio3': [{ required: true, message: '请输入系数', trigger: 'blur' }, { pattern: /^((0\.[1-9]{0,1}?)|1|1.0|0)$/, message: '请输入0.1-1之间的数字，最多保留1位小数', trigger: 'blur' }]
+        'extraAttr.ratio2': [{ required: true, message: '请输入系数', trigger: 'blur' }, { pattern: /^((0\.[1-9]{0,1}?)|1|1.0)$/, message: '请输入0.1-1之间的数字，最多保留1位小数', trigger: 'blur' }],
+        'extraAttr.ratio3': [{ required: true, message: '请输入系数', trigger: 'blur' }, { pattern: /^((0\.[1-9]{0,1}?)|1|1.0)$/, message: '请输入0.1-1之间的数字，最多保留1位小数', trigger: 'blur' }]
       },
       //充电时间选择
       chargeTimeMax: 0,
